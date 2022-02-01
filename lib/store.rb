ebc98e065c1195ec1d71dd2518b98b838426f8ac
Emily-Waters <emilyabigailwaters@gmail.com>
2022-02-01 02:21:32 +0000

    Complete exercise_9.rb, Cleanup previous exercises

--- lib/store.rb
+++ lib/store.rb
@@ -4,11 +4,19 @@ class Store < ActiveRecord::Base
   validates :name, presence: true, length:{ minimum: 3 }
   validates :annual_revenue,  numericality: { only_integer: true, greater_than: 0 }
   validate :men_or_womens_apparel
+  before_destroy :safe_to_destroy?
+
 
   def men_or_womens_apparel
     if !self.womens_apparel && !self.mens_apparel
       errors.add :men_or_womens_apparel, message:"Must sell either mens or womens apparel or both"
     end
+  end
+
+  private
+
+  def safe_to_destroy?
+    self.employees.count < 1
   end
 
 end
